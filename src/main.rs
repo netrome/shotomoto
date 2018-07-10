c4fd9797c1df018f8f0741407958a7cd0c6a7c0d
Marten <marten3@kth.se>
2018-07-10 20:25:32 +0200

    Killing enabled

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate piston_window;
 
 use piston_window::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    println!("Hello, world!");
-
     let mut window: PistonWindow = WindowSettings::new("Hello World!", [800, 400])
         .exit_on_esc(true).build().unwrap();
 
-    let mut s1 = Ship::new(50., 50., 10., 100.);
-    let mut s2 = Ship::new(250., 250., 40., 100.);
-    let shots: Arc<Mutex<Vec<Shot>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut s1 = Ship::new(50., 200., 10., 1.);
+    let mut s2 = Ship::new(750., 200., 190., 1.);
+    let shots1: Arc<Mutex<VecDeque<Shot>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let shots2: Arc<Mutex<VecDeque<Shot>>> = Arc::new(Mutex::new(VecDeque::new()));
+
+    for _ in 1..5{
+        shots1.lock().unwrap().push_back(Shot{x: 0., y: 0., dx: 0., dy: 0.});
+        shots2.lock().unwrap().push_back(Shot{x: 0., y: 0., dx: 0., dy: 0.});
+    }
 
     s1.controls.insert(Key::W, Command::JumpF);
     s1.controls.insert(Key::A, Command::JumpL);
@@ -20,7 +24,7 @@ fn main() {
     s1.controls.insert(Key::D, Command::JumpR);
     s1.controls.insert(Key::Q, Command::RotL);
     s1.controls.insert(Key::E, Command::RotR);
-    s1.controls.insert(Key::Space, Command::Shoot);
+    s1.controls.insert(Key::R, Command::Shoot);
 
     s2.controls.insert(Key::I, Command::JumpF);
     s2.controls.insert(Key::J, Command::JumpL);
@@ -28,24 +32,39 @@ fn main() {
     s2.controls.insert(Key::L, Command::JumpR);
     s2.controls.insert(Key::U, Command::RotL);
     s2.controls.insert(Key::O, Command::RotR);
+    s2.controls.insert(Key::P, Command::Shoot);
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
             clear([0.1, 0.2, 0.1, 0.5], g);
-            rectangle([1., 0., 0., 1.], [0., 0., 40., 40.], c.transform.trans(s1.x, s1.y).rot_deg(s1.rot).trans(-20., -20.), g);
-            rectangle([0., 0., 1., 1.], [0., 0., 40., 40.], c.transform.trans(s2.x, s2.y).rot_deg(s2.rot).trans(-20., -20.), g);
+            rectangle([s1.health as f32, 0.1, 0.1, 1.], [0., 0., 40., 40.], c.transform.trans(s1.x, s1.y).rot_deg(s1.rot).trans(-20., -20.), g);
+            rectangle([0.1, 0.1, s2.health as f32, 1.], [0., 0., 40., 40.], c.transform.trans(s2.x, s2.y).rot_deg(s2.rot).trans(-20., -20.), g);
             line([0., 1., 1., 1.], 3., [s1.x, s1.y, s1.x + 50.*s1.rot.to_radians().cos(), s1.y + 50.*s1.rot.to_radians().sin()], c.transform, g);
             line([0., 1., 1., 1.], 3., [s2.x, s2.y, s2.x + 50.*s2.rot.to_radians().cos(), s2.y + 50.*s2.rot.to_radians().sin()], c.transform, g);
 
-            for shot in shots.lock().unwrap().iter_mut() {
+            for shot in shots1.lock().unwrap().iter_mut().chain(shots2.lock().unwrap().iter_mut()) {
                 rectangle([0.6, 0.6, 0.6, 1.], [0., 0., 5., 5.], c.transform.trans(shot.x, shot.y), g);
                 shot.x += shot.dx;
                 shot.y += shot.dy;
             }
+
+            for shot in shots1.lock().unwrap().iter(){
+                let diff = (shot.x - s2.x).powi(2) + (shot.y - s2.y).powi(2);
+                if diff < 400. {
+                    s2.health -= 0.01;
+                }
+            }
+
+            for shot in shots2.lock().unwrap().iter(){
+                let diff = (shot.x - s1.x).powi(2) + (shot.y - s1.y).powi(2);
+                if diff < 400. {
+                    s1.health -= 0.01;
+                }
+            }
         });
 
         if let Some(Button::Keyboard(key)) = e.press_args() {
-            println!("{:?}", key);
+            //println!("{:?}", key);
             if let Some(command) = s1.parse_key(key){
                 s1.command(command);
             }
@@ -56,12 +75,30 @@ fn main() {
 
             if s1.shooting {
                 s1.shooting = false;
-                let dx = 2.*s1.rot.to_radians().cos();
-                let dy = 2.*s1.rot.to_radians().sin();
-                shots.lock().unwrap().push( Shot{ x: s1.x + 10.*dx, y: s1.y + 10.*dy, dx, dy });
+                let dx = 4.*s1.rot.to_radians().cos();
+                let dy = 4.*s1.rot.to_radians().sin();
+                shots1.lock().unwrap().pop_front();
+                shots1.lock().unwrap().push_back( Shot{ x: s1.x + 10.*dx, y: s1.y + 10.*dy, dx, dy });
+            }
+
+            if s2.shooting {
+                s2.shooting = false;
+                let dx = 4.*s2.rot.to_radians().cos();
+                let dy = 4.*s2.rot.to_radians().sin();
+                shots2.lock().unwrap().pop_front();
+                shots2.lock().unwrap().push_back( Shot{ x: s2.x + 10.*dx, y: s2.y + 10.*dy, dx, dy });
             }
         }
+
+        if s1.health < 0. || s2.health < 0. {
+            break;
+        }
+
     }
+
+    println!("Game over");
+    println!("Ship red health: {}", s1.health);
+    println!("Ship blue health: {}", s2.health);
 }
 
 struct Ship{
