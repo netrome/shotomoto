415535137dd7baa43df67684935d951bef980a51
Marten <marten3@kth.se>
2018-07-10 19:50:29 +0200

    Simple shooting implemented

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate piston_window;
 
 use piston_window::*;
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     println!("Hello, world!");
@@ -11,6 +12,7 @@ fn main() {
 
     let mut s1 = Ship::new(50., 50., 10., 100.);
     let mut s2 = Ship::new(250., 250., 40., 100.);
+    let shots: Arc<Mutex<Vec<Shot>>> = Arc::new(Mutex::new(Vec::new()));
 
     s1.controls.insert(Key::W, Command::JumpF);
     s1.controls.insert(Key::A, Command::JumpL);
@@ -18,6 +20,7 @@ fn main() {
     s1.controls.insert(Key::D, Command::JumpR);
     s1.controls.insert(Key::Q, Command::RotL);
     s1.controls.insert(Key::E, Command::RotR);
+    s1.controls.insert(Key::Space, Command::Shoot);
 
     s2.controls.insert(Key::I, Command::JumpF);
     s2.controls.insert(Key::J, Command::JumpL);
@@ -33,6 +36,12 @@ fn main() {
             rectangle([0., 0., 1., 1.], [0., 0., 40., 40.], c.transform.trans(s2.x, s2.y).rot_deg(s2.rot).trans(-20., -20.), g);
             line([0., 1., 1., 1.], 3., [s1.x, s1.y, s1.x + 50.*s1.rot.to_radians().cos(), s1.y + 50.*s1.rot.to_radians().sin()], c.transform, g);
             line([0., 1., 1., 1.], 3., [s2.x, s2.y, s2.x + 50.*s2.rot.to_radians().cos(), s2.y + 50.*s2.rot.to_radians().sin()], c.transform, g);
+
+            for shot in shots.lock().unwrap().iter_mut() {
+                rectangle([0.6, 0.6, 0.6, 1.], [0., 0., 5., 5.], c.transform.trans(shot.x, shot.y), g);
+                shot.x += shot.dx;
+                shot.y += shot.dy;
+            }
         });
 
         if let Some(Button::Keyboard(key)) = e.press_args() {
@@ -44,6 +53,13 @@ fn main() {
             if let Some(command) = s2.parse_key(key){
                 s2.command(command);
             }
+
+            if s1.shooting {
+                s1.shooting = false;
+                let dx = 2.*s1.rot.to_radians().cos();
+                let dy = 2.*s1.rot.to_radians().sin();
+                shots.lock().unwrap().push( Shot{ x: s1.x + 10.*dx, y: s1.y + 10.*dy, dx, dy });
+            }
         }
     }
 }
@@ -54,6 +70,15 @@ struct Ship{
     rot: f64,
     health: f64,
     controls: HashMap<Key, Command>,
+    shooting: bool,
+}
+
+#[derive(Clone)]
+struct Shot{
+    x: f64,
+    y: f64,
+    dx: f64,
+    dy: f64,
 }
 
 #[derive(Clone)]
@@ -64,12 +89,13 @@ enum Command{
     JumpB,
     RotR,
     RotL,
+    Shoot,
 }
 
 impl Ship{
     fn new(x: f64, y: f64, rot: f64, health: f64) -> Self{
         let controls = HashMap::new();
-        Self{ x, y, rot, health, controls}
+        Self{ x, y, rot, health, controls, shooting: false}
     }
 
     fn parse_key(&self, key: Key) -> Option<Command> {
@@ -83,10 +109,11 @@ impl Ship{
         match command {
             Command::JumpF => self.jump(0.),
             Command::JumpB => self.jump(-180.),
-            Command::JumpR => self.jump(-90.),
-            Command::JumpL => self.jump(90.),
+            Command::JumpR => self.jump(90.),
+            Command::JumpL => self.jump(-90.),
             Command::RotR => self.rot(13.),
             Command::RotL => self.rot(-13.),
+            Command::Shoot => self.shoot(),
         }
     }
 
@@ -101,8 +128,12 @@ impl Ship{
 
         let clamped = self.x.max(0.).min(800.);
         self.x = clamped;
-        let clamped = self.y.max(0.).min(800.);
+        let clamped = self.y.max(0.).min(400.);
         self.y = clamped;
+    }
+
+    fn shoot(&mut self){
+        self.shooting = true;
     }
 }
 
